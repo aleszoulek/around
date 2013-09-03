48d7a8d6119ec839876967aaebeadfc2c14888c0
Jakub Vysoky <jakub@borka.cz>
2013-09-03 22:54:52 +0200

    first soft and easy roudnice scraping
    wip..

---
+++ around/spiders/roudnice.py
@@ -0,0 +1,38 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+
+from around.items import EventItem
+
+class RoudniceSpider(BaseSpider):
+    name = "roudnice"
+    allowed_domains = ["roudnicenl.cz"]
+    start_urls = [
+        'http://www.roudnicenl.cz/mesto/kalendar-akci',
+    ]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        events = hxs.select("//div[@id='floatbug']/div/div")
+        event_items = []
+        for event in events:
+            if not event.select("h3"):
+                continue
+
+            name = event.select("a/@name").extract()[0]
+            data = event.select("a/span[@class='datum']/text()").extract()[0].split('|')
+
+            event_item = EventItem()
+            event_item['name'] = event.select('h3/span/text()').extract()[0]
+            event_item['source'] = self.name
+            event_item['description'] = ''.join(event.select('text()').extract()).strip()
+            event_item['link'] = '%s#%s' % (self.start_urls[0], name)
+            event_item['id'] = name
+
+            if data[1].endswith('         '):
+                event_item['venue'] = data[2].strip()
+            else:
+                event_item['venue'] = data[1].strip()
+
+            event_items.append(event_item)
+        return event_items
+
