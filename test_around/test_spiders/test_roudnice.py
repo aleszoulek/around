cdce55b85b513e20a7db4921a6ef0b0fd3ccca17
Jakub Vysoky <jakub@borka.cz>
2013-09-13 19:41:06 +0200

    test venue and description and more complex html

--- test_around/test_spiders/test_roudnice.py
+++ test_around/test_spiders/test_roudnice.py
@@ -26,18 +26,25 @@ class TestRoudniceSpider(TestCase):
         spider = roudnice.RoudniceSpider()
         xhs = HtmlXPathSelector(text=self.html)
         nodes = list(spider.get_nodes(xhs))
-        # TODO: add venue, description
         expected = {
             'id': '4789',
             'link': 'http://www.roudnicenl.cz/mesto/kalendar-akci#4789',
             'name': 'Letci z hlubin',
+            'venue': u'Pod\u0159ipsk\xe9 muzeum',
+            'description': u'V\xfdstava  p\u0159ibli\u017euje v\xfdznamnou bitvu mezi americk\xfdm letectvem a n\u011bmeckou Lufthaffe z roku 1944 nad Kru\u0161n\xfdmi horami a prezentuje autentick\xe9 expon\xe1ty v podob\u011b relikt\u016f sest\u0159elen\xfdch letadel stejn\u011b jako dal\u0161\xed historick\xe9 re\xe1lie vztahuj\xedc\xed se k t\xe9to v\xfdznamn\xe9 ud\xe1losti 2. sv\u011btov\xe9 v\xe1lky.\r\n\r\nVernis\xe1\u017e v\xfdstavy: 8. 4. v 17.30 hodin.',
         }
-        got = spider.parse_value(nodes[0])
-        tools.assert_equals(expected['id'], got['id'])
-        tools.assert_equals(expected['link'], got['link'])
-        tools.assert_equals(expected['name'], got['name'])
+        tools.assert_equals(expected, spider.parse_value(nodes[0]))
 
-    def test_parse_value_with_more_complicated_data(self):
-        # TODO: ^
-        pass
+    def test_parse_value_with_time_and_more_info_get_proper_data(self):
+        spider = roudnice.RoudniceSpider()
+        xhs = HtmlXPathSelector(text=self.html)
+        nodes = list(spider.get_nodes(xhs))
+        expected = {
+            'id': '4910',
+            'link': 'http://www.roudnicenl.cz/mesto/kalendar-akci#4910',
+            'name': u'Podzimn\xed tvo\u0159en\xed z list\u016f',
+            'venue': u'Galerie modern\xedho um\u011bn\xed',
+            'description': u'Sobotn\xed v\xfdtvarn\xe1 d\xedlna pro dosp\u011bl\xe9 a rodi\u010de s d\u011btmi. Z nasb\xedran\xfdch list\u016f budeme vytv\xe1\u0159et kol\xe1\u017ee, podzimn\xed v\u011bnce nebo r\u016f\u017ee.',
+        }
+        tools.assert_equals(expected, spider.parse_value(nodes[-3]))
 
