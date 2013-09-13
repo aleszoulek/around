de382d76a3e3bf671066e45df927c99d0ff5b3fb
Jakub Vysoky <jakub@borka.cz>
2013-09-13 23:30:09 +0200

    parsing dates and times

--- around/spiders/roudnice.py
+++ around/spiders/roudnice.py
@@ -1,3 +1,5 @@
+import datetime
+
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 
@@ -17,16 +19,42 @@ class RoudniceSpider(BaseSpider):
                 continue
             yield event
 
+    def parse_date(self, date, time=None):
+        if '-' in date:
+            date_from, date_to = date.split('-')
+            date_from = datetime.datetime.strptime(date_from.strip(), '%d. %m. %Y').date()
+            date_to = datetime.datetime.strptime(date_to.strip(), '%d. %m. %Y').date()
+        else:
+            date = datetime.datetime.strptime(date.strip(), '%d. %m. %Y').date()
+            date_from, date_to = date, date
+
+        time_from, time_to = None, None
+        if time:
+            time = datetime.datetime.strptime(time, '%H:%M').time()
+            time_from, time_to = time, time
+
+        return date_from, date_to, time_from, time_to
+
     def parse_value(self, node):
         name = node.select("a/@name").extract()[0]
         data = node.select("a/span[@class='datum']/text()").extract()[0].split('|')
+
+        date = data[0]
+        time = None
         if data[1].endswith('         '):
             venue = data[2].strip()
+            time = data[1].strip()
         else:
             venue = data[1].strip()
 
+        date_from, date_to, time_from, time_to = self.parse_date(date, time)
+
         return {
             'id': name,
+            'date_from': date_from,
+            'date_to': date_to,
+            'time_from': time_from,
+            'time_to': time_to,
             'name': node.select('h3/span/text()').extract()[0],
             'description': ''.join(node.select('text()').extract()).strip(),
             'link': '%s#%s' % (self.start_urls[0], name),
--- test_around/test_spiders/test_roudnice.py
+++ test_around/test_spiders/test_roudnice.py
@@ -1,6 +1,7 @@
 from nose import tools
 from unittest import TestCase
 from os import path
+from datetime import date, time
 
 from scrapy.selector import HtmlXPathSelector
 
@@ -29,6 +30,10 @@ class TestRoudniceSpider(TestCase):
         expected = {
             'id': '4789',
             'link': 'http://www.roudnicenl.cz/mesto/kalendar-akci#4789',
+            'date_from': date(2013, 4, 8),
+            'date_to': date(2013, 9, 29),
+            'time_from': None,
+            'time_to': None,
             'name': 'Letci z hlubin',
             'venue': u'Pod\u0159ipsk\xe9 muzeum',
             'description': u'V\xfdstava  p\u0159ibli\u017euje v\xfdznamnou bitvu mezi americk\xfdm letectvem a n\u011bmeckou Lufthaffe z roku 1944 nad Kru\u0161n\xfdmi horami a prezentuje autentick\xe9 expon\xe1ty v podob\u011b relikt\u016f sest\u0159elen\xfdch letadel stejn\u011b jako dal\u0161\xed historick\xe9 re\xe1lie vztahuj\xedc\xed se k t\xe9to v\xfdznamn\xe9 ud\xe1losti 2. sv\u011btov\xe9 v\xe1lky.\r\n\r\nVernis\xe1\u017e v\xfdstavy: 8. 4. v 17.30 hodin.',
@@ -42,9 +47,29 @@ class TestRoudniceSpider(TestCase):
         expected = {
             'id': '4910',
             'link': 'http://www.roudnicenl.cz/mesto/kalendar-akci#4910',
+            'date_from': date(2013, 11, 23),
+            'date_to': date(2013, 11, 23),
+            'time_from': time(0, 0),
+            'time_to': time(0, 0),
             'name': u'Podzimn\xed tvo\u0159en\xed z list\u016f',
             'venue': u'Galerie modern\xedho um\u011bn\xed',
             'description': u'Sobotn\xed v\xfdtvarn\xe1 d\xedlna pro dosp\u011bl\xe9 a rodi\u010de s d\u011btmi. Z nasb\xedran\xfdch list\u016f budeme vytv\xe1\u0159et kol\xe1\u017ee, podzimn\xed v\u011bnce nebo r\u016f\u017ee.',
         }
         tools.assert_equals(expected, spider.parse_value(nodes[-3]))
+
+    def test_parse_date_with_range_give_two_datetime_objects(self):
+        spider = roudnice.RoudniceSpider()
+        date_from, date_to, time_from, time_to = spider.parse_date('18. 09. 2013 - 22. 09. 2013')
+        tools.assert_equals(date(2013, 9, 18), date_from)
+        tools.assert_equals(date(2013, 9, 22), date_to)
+        tools.assert_is_none(time_from)
+        tools.assert_is_none(time_to)
+
+    def test_parse_date_with_time_fill_time_from_and_time_to(self):
+        spider = roudnice.RoudniceSpider()
+        date_from, date_to, time_from, time_to = spider.parse_date('23. 11. 2013', '14:00')
+        tools.assert_equals(date(2013, 11, 23), date_from)
+        tools.assert_equals(date(2013, 11, 23), date_to)
+        tools.assert_equals(time(14, 0), time_from)
+        tools.assert_equals(time(14, 0), time_to)
 
