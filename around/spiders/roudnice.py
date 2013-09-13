743f49c76dc85352a14262c892aa3ca02c9b278b
Jakub Vysoky <jakub@borka.cz>
2013-09-13 11:10:01 +0200

    test some real values from roudnice html

--- around/spiders/roudnice.py
+++ around/spiders/roudnice.py
@@ -11,35 +11,35 @@ class RoudniceSpider(BaseSpider):
     ]
 
     def get_nodes(self, hxs):
-        for n in hxs.select('//ul/li'):
-            yield n
-
-    def parse_value(select, node):
-        return node.select('text()').extract()[0]
-
-    def parse(self, response):
-        hxs = HtmlXPathSelector(response)
         events = hxs.select("//div[@id='floatbug']/div/div")
-        event_items = []
         for event in events:
             if not event.select("h3"):
                 continue
+            yield event
 
-            name = event.select("a/@name").extract()[0]
-            data = event.select("a/span[@class='datum']/text()").extract()[0].split('|')
+    def parse_value(self, node):
+        name = node.select("a/@name").extract()[0]
+        data = node.select("a/span[@class='datum']/text()").extract()[0].split('|')
+        if data[1].endswith('         '):
+            venue = data[2].strip()
+        else:
+            venue = data[1].strip()
 
+        return {
+            'id': name,
+            'name': node.select('h3/span/text()').extract()[0],
+            'description': ''.join(node.select('text()').extract()).strip(),
+            'link': '%s#%s' % (self.start_urls[0], name),
+            'venue': venue,
+        }
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        event_items = []
+        for event in self.get_nodes(hxs):
             event_item = EventItem()
-            event_item['name'] = event.select('h3/span/text()').extract()[0]
+            event_item.update(self.parse_value(event))
             event_item['source'] = self.name
-            event_item['description'] = ''.join(event.select('text()').extract()).strip()
-            event_item['link'] = '%s#%s' % (self.start_urls[0], name)
-            event_item['id'] = name
-
-            if data[1].endswith('         '):
-                event_item['venue'] = data[2].strip()
-            else:
-                event_item['venue'] = data[1].strip()
-
             event_items.append(event_item)
         return event_items
 
--- test_around/test_spiders/test_roudnice.py
+++ test_around/test_spiders/test_roudnice.py
@@ -1,30 +1,43 @@
 from nose import tools
 from unittest import TestCase
+from os import path
 
 from scrapy.selector import HtmlXPathSelector
 
 from around.spiders import roudnice
+import test_around
+
+html = path.join(path.dirname(test_around.__file__), 'data', 'roudnice-130912-2324.html')
+html = open(html).read()
 
 class TestRoudniceSpider(TestCase):
     def setUp(self):
-        self.html = '''
-        <body>
-        <ul>
-            <li>one</li>
-            <li>two</li>
-        </ul>
-        </body>
-        '''
+        self.html = html
 
-    def test_get_nodes_will_get_two_nodes(self):
+    def test_get_nodes_get_seventy_nodes(self):
+        # this info comes from the shell::
+        #   links -dump test_around/data/roudnice-130912-2324.html | \
+        #   grep -- '---------------------------------------------' | wc -l
         spider = roudnice.RoudniceSpider()
         xhs = HtmlXPathSelector(text=self.html)
-        tools.assert_equals(2, len(list(spider.get_nodes(xhs))))
+        tools.assert_equals(70, len(list(spider.get_nodes(xhs))))
 
-    def test_parse_value_get_one_and_two_strings(self):
+    def test_parse_value_with_just_venue_only_get_proper_data(self):
         spider = roudnice.RoudniceSpider()
         xhs = HtmlXPathSelector(text=self.html)
         nodes = list(spider.get_nodes(xhs))
-        tools.assert_equals('one', spider.parse_value(nodes[0]))
-        tools.assert_equals('two', spider.parse_value(nodes[-1]))
+        # TODO: add venue, description
+        expected = {
+            'id': '4789',
+            'link': 'http://www.roudnicenl.cz/mesto/kalendar-akci#4789',
+            'name': 'Letci z hlubin',
+        }
+        got = spider.parse_value(nodes[0])
+        tools.assert_equals(expected['id'], got['id'])
+        tools.assert_equals(expected['link'], got['link'])
+        tools.assert_equals(expected['name'], got['name'])
+
+    def test_parse_value_with_more_complicated_data(self):
+        # TODO: ^
+        pass
 
